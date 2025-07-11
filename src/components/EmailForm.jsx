08e7371d21719533b26295a0b5d0a132be402c32
RJ <103343995+RJPhenom@users.noreply.github.com>
2025-07-11 15:25:19 -0400

    Update

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -12,10 +12,6 @@ import { useState } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-function clearForm() {
-  set;
-}
-
 export default function EmailForm({
   selectedEmail,
   editMode,
